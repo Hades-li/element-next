dc3a23252f409ada2d7c0bf750a5dd49da68dff6
plus li <121318100@gmail.com>
2020-06-30 17:32:02 +0800

    add eslint

--- packages/scrollbar/src/main.js
+++ packages/scrollbar/src/main.js
@@ -1,6 +1,6 @@
 // reference https://github.com/noeldelgado/gemini-scrollbar/blob/master/index.js
 
-import {h} from 'vue'
+import {h,ref} from 'vue'
 import { addResizeListener, removeResizeListener } from 'src/utils/resize-event';
 import scrollbarWidth from 'src/utils/scrollbar-width';
 import { toObject } from 'src/utils/util';
@@ -25,79 +25,86 @@ export default {
     }
   },
 
-  data() {
+  /*data() {
     return {
       sizeWidth: '0',
       sizeHeight: '0',
       moveX: 0,
       moveY: 0
     };
-  },
+  },*/
 
   computed: {
     wrap() {
       return this.$refs.wrap;
     }
   },
+  setup(props, ctx) {
+    const sizeWidth =  ref('0')
+    const sizeHeight =  ref('0')
+    const moveX =  ref(0)
+    const moveY =  ref(0)
 
-  render() {
-    let gutter = scrollbarWidth();
-    let style = this.wrapStyle;
+    function render() {
+      let gutter = scrollbarWidth();
+      let style = props.wrapStyle;
 
-    if (gutter) {
-      const gutterWith = `-${gutter}px`;
-      const gutterStyle = `margin-bottom: ${gutterWith}; margin-right: ${gutterWith};`;
+      if (gutter) {
+        const gutterWith = `-${gutter}px`;
+        const gutterStyle = `margin-bottom: ${gutterWith}; margin-right: ${gutterWith};`;
 
-      if (Array.isArray(this.wrapStyle)) {
-        style = toObject(this.wrapStyle);
-        style.marginRight = style.marginBottom = gutterWith;
-      } else if (typeof this.wrapStyle === 'string') {
-        style += gutterStyle;
-      } else {
-        style = gutterStyle;
+        if (Array.isArray(props.wrapStyle)) {
+          style = toObject(props.wrapStyle);
+          style.marginRight = style.marginBottom = gutterWith;
+        } else if (typeof props.wrapStyle === 'string') {
+          style += gutterStyle;
+        } else {
+          style = gutterStyle;
+        }
       }
-    }
-    const view = h(this.tag, {
-      class: ['el-scrollbar__view', this.viewClass],
-      style: this.viewStyle,
-      ref: 'resize'
-    }, this.$slots.default);
-    const wrap = (
-      <div
-        ref="wrap"
-        style={ style }
-        onScroll={ this.handleScroll }
-        class={ [this.wrapClass, 'el-scrollbar__wrap', gutter ? '' : 'el-scrollbar__wrap--hidden-default'] }>
-        { [view] }
-      </div>
-    );
-    let nodes;
-
-    if (!this.native) {
-      nodes = ([
-        wrap,
-        <Bar
-          move={ this.moveX }
-          size={ this.sizeWidth }>
-
-        </Bar>,
-        <Bar
-          vertical
-          move={ this.moveY }
-          size={ this.sizeHeight }>
-        </Bar>
-      ]);
-    } else {
-      nodes = ([
+      const view = h(props.tag, {
+        class: ['el-scrollbar__view', this.viewClass],
+        style: props.viewStyle,
+        ref: 'resize'
+      }, ctx.slots.default());
+      const wrap = (
         <div
           ref="wrap"
-          class={ [this.wrapClass, 'el-scrollbar__wrap'] }
-          style={ style }>
+          style={ style }
+          onScroll={ this.handleScroll }
+          class={ [this.wrapClass, 'el-scrollbar__wrap', gutter ? '' : 'el-scrollbar__wrap--hidden-default'] }>
           { [view] }
         </div>
-      ]);
+      );
+      let nodes;
+
+      if (!this.native) {
+        nodes = ([
+          wrap,
+          <Bar
+            move={ this.moveX }
+            size={ this.sizeWidth }>
+
+          </Bar>,
+          <Bar
+            vertical
+            move={ this.moveY }
+            size={ this.sizeHeight }>
+          </Bar>
+        ]);
+      } else {
+        nodes = ([
+          <div
+            ref="wrap"
+            class={ [this.wrapClass, 'el-scrollbar__wrap'] }
+            style={ style }>
+            { [view] }
+          </div>
+        ]);
+      }
+      return h('div', { class: 'el-scrollbar' }, nodes);
     }
-    return h('div', { class: 'el-scrollbar' }, nodes);
+    return render
   },
 
   methods: {
